92d85d6adc3497393b0dac91767f6ffe94a1cea2
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2025-12-01 16:54:37 +0530

    package-bug

--- src/components/brands.tsx
+++ src/components/brands.tsx
@@ -1,16 +1,17 @@
-'use client'
-import { LogosCarousel, CarouselItem } from "logos-carousel-react"
+import Marquee from "react-fast-marquee"
+import Image from "next/image"
 export default function Brands() {
     return (
         <>
             <section className="relative max-w-6xl mx-auto my-6 invert brightness-50 opacity-40">
-                <LogosCarousel className="h-[40px]">
-                    <CarouselItem src="https://www.svgrepo.com/show/305632/zoom.svg" alt="Logo 4" title="Logo 4" />
-                    <CarouselItem src="https://www.svgrepo.com/show/305750/asus.svg" alt="Logo 4" title="Logo 4" />
-                    <CarouselItem src="https://www.svgrepo.com/show/305754/audi.svg" alt="Logo 4" title="Logo 4" />
-                    <CarouselItem src="https://www.svgrepo.com/show/305861/citrix.svg" alt="Logo 4" title="Logo 4" />
+               
+                <Marquee autoFill loop={0} speed={10} direction="left" className="">
+                    <Image className="px-4" src={'https://www.svgrepo.com/show/305632/zoom.svg'} alt="logo" height={160} width={160}></Image>
+                    <Image className="px-4" src={'https://www.svgrepo.com/show/305750/asus.svg'} alt="logo" height={160} width={160}></Image>
+                    <Image className="px-4" src={'https://www.svgrepo.com/show/305754/audi.svg'} alt="logo" height={160} width={160}></Image>
+                    <Image className="px-4" src={'https://www.svgrepo.com/show/305861/citrix.svg'} alt="logo" height={160} width={160}></Image>
 
-                </LogosCarousel>
+                </Marquee>
 
             </section>
 
